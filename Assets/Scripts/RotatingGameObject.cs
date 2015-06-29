4419a26323711f6e480161930456152b69fa26b5
Matic Leva <vertical3life@gmail.com>
2015-06-29 22:20:21 +0200

    Rotating GameObject utility script

---
+++ Assets/Scripts/RotatingGameObject.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class RotatingGameObject : MonoBehaviour {
+
+    public Int32 speed;
+    private Vector3 rotation = Vector3.zero;
+
+    void Start()
+    {
+        rotation = new Vector3(0, 0, speed);
+    }
+	void Update () {
+        transform.Rotate(rotation);
+	}
+}
